f87d03935111a8751232757a58b359b75356fec1
TaotiePing <Mr.taotie@hotmail.com>
2019-06-25 20:31:09 +0800

    =修改readme

---
+++ precache-manifest.acef75073bcd5cf039eae3db84c27ef7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45c1db3f82eb78eb1177d0d3198d06ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6cabdf4162728261b14",
+    "url": "/static/css/main.0c9e50ab.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "d6cabdf4162728261b14",
+    "url": "/static/js/main.869207f9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1b0dec6825a7dbec79e7d3a8f04ad890",
+    "url": "/static/media/logo.1b0dec68.svg"
+  }
+]);
